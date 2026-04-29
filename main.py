b6748a8492810ffc80340dd7e26a2dbd8283a96a
Hunter Li <hunterisaac2009@gmail.com>
2026-04-28 19:33:28 -0700

    WIP - Validating LLM Response
    Going pretty smooth so far lol

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
+#trying to figure out what code my transpiler should create! :)
 import os
-
-api_key = os.environ.get("API_KEY")
+from typing import Literal
+from pydantic import BaseModel, ConfigDict, ValidationError
 from litellm import completion
 import os
-
+import json 
+api_key = os.environ.get("API_KEY")
 os.environ["MISTRAL_API_KEY"] = api_key
 system_prompt = 'You are a mathematical genius. You can only respond with one of two formats: one for calling tools: {"state": "tool", "tool_name": "tool", "tool_args": {"a": x, "b": y} } or one for stating an answer: {"state": "final", "final_answer": "blah blah blah"} Respond only in JSON'
 user_prompt = 'Hello, how are you?'
@@ -12,5 +14,10 @@ response = completion(
   messages=[{"role": "user", "content": f"System Prompt: {system_prompt} Prompt: {user_prompt}"}]
 )
 response = response.choices[0].message.content
-
-print(response[7:-3].strip())
+data = json.loads(response[7:-3].strip())
+class LLMResponse(BaseModel): #validating llm output ... how do i run it through lol
+    state: Literal['tool', 'final']
+try:
+  LLMResponse.model_validate(data)  
+except ValidationError as e:
+  print(e)
