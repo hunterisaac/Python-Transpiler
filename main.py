be3f089f29cc41dbe544fd16343f6d14453e891d
Hunter Li <hunterisaac2009@gmail.com>
2026-04-28 19:00:25 -0700

    Added system prompt + user prompt
    yeah just defined some stuffs

--- main.py
+++ main.py
@@ -5,9 +5,10 @@ from litellm import completion
 import os
 
 os.environ["MISTRAL_API_KEY"] = api_key
-
+system_prompt = 'You are a mathematical genius. You can only respond with one of two formats: one for calling tools: {"state": "tool", "tool_name": "tool", "tool_args": {"a": x, "b": y} } or one for stating an answer: {"state": "final", "final_answer": "blah blah blah"} Respond only in JSON'
+user_prompt = 'Hello, how are you?'
 response = completion(
   model="mistral/mistral-tiny",
-  messages=[{"role": "user", "content": "Hello, how are you?"}]
+  messages=[{"role": "user", "content": f"System Prompt: {system_prompt} Prompt: {user_prompt}"}]
 )
 print(response.choices[0].message.content)
