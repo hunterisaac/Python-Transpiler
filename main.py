59b2dc3916173534c45f25c81b8d23db731cccb7
Hunter Li <hunterisaac2009@gmail.com>
2026-04-30 18:25:55 -0700

    Saving progress on my tool system
    yep kinda messy rn but dont wanna lose it

--- main.py
+++ main.py
@@ -12,7 +12,30 @@ system_prompt = 'You are a mathematical genius. You can only respond with one of
 user_prompt = 'Hello, how are you?'
 
 message_history = [{"role": "system", "content": system_prompt}, {"role": "user", "content": user_prompt}]
+#Storing tool calls using decorators
+tool_list = []
 
+def tool(func):
+   tool_list.append({'tool_name': func.__name__, 'description': func.__doc__, 'function': func  }) #stores with tool name, desecription, and the callable function.
+@tool
+def add(numbers):
+   """Tool to add all # in a dict"""
+   total = 0
+   for x in numbers:
+      total += numbers[x]
+   return total
+@tool
+def subtract(numbers):
+   """Tool to subtract all # in a dict"""
+   total = 0
+   for x in numbers:
+      total -= numbers[x]
+   return total
+
+
+      
+#Executing Tool Calls
+#Registering tool calls
 class ToolResponse(BaseModel):
     state: Literal['tool']
     tool_name: str
@@ -28,22 +51,25 @@ while True:
     messages=message_history
   )
   response = response.choices[0].message.content
-  message_history.append(response)
+  message_history.append({"role":"assistant", "content": response})
   print(message_history)
   try:
     amount_chars = len(response) #getting json
     start_index = response.index("{") 
     end_index = response.rfind("}", start_index, amount_chars)
     data = json.loads(response[start_index:end_index+1])
+    try:
+      response = LLMResponse(response_type=data)
+      result = response.response_type #need to extract data from response
+      if result.state == "final":
+        print(result.final_answer)
+        break
+      if result.state =="tool":
+         
+    except ValidationError as e:
+      message_history.append({"role": "system", "content": e})
   except Exception as e:
-     message_history.append({"role": "system_response", "content": "Invalid JSON"})
+     message_history.append({"role": "system", "content": "Invalid JSON"})
 
   
-  try:
-    response = LLMResponse(response_type=data)
-    result = response.response_type #need to extract data from response
-    if result.state == "final":
-      print(result.final_answer)
-      break
-  except ValidationError as e:
-    print(e)
+  
---
+++ testing.py
@@ -0,0 +1,17 @@
+tool_list = []
+def tool(func):
+   tool_list.append({f"tool_name": func.__name__, 'description': func.__doc__, 'function': func  }) #stores with tool name, desecription, and the callable function.
+@tool
+def add(numbers):
+   """Tool to add all # in a dict"""
+   total = 0
+   for x in numbers:
+      total += numbers[x]
+   return total
+@tool
+def subtract(numbers):
+   """Tool to subtract all # in a dict"""
+   total = 0
+   for x in numbers:
+      total -= numbers[x]
+   return total
