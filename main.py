9c4b2f0cc4ce3af6bd1728cf80f9da4652784628
Hunter Li <hunterisaac2009@gmail.com>
2026-04-30 18:40:19 -0700

    Got tool calls working
    Changed to dict to make it easier
    Need to find a way to omit the functions itself i think.

--- main.py
+++ main.py
@@ -8,15 +8,11 @@ import json
 api_key = os.environ.get("API_KEY")
 os.environ["MISTRAL_API_KEY"] = api_key
 
-system_prompt = 'You are a mathematical genius. You can only respond with one of two formats: one for calling tools: {"state": "tool", "tool_name": "tool", "tool_args": {"a": x, "b": y} } or one for stating an answer: {"state": "final", "final_answer": "blah blah blah"} Respond only in JSON'
-user_prompt = 'Hello, how are you?'
-
-message_history = [{"role": "system", "content": system_prompt}, {"role": "user", "content": user_prompt}]
 #Storing tool calls using decorators
-tool_list = []
 
+tool_list = {}
 def tool(func):
-   tool_list.append({'tool_name': func.__name__, 'description': func.__doc__, 'function': func  }) #stores with tool name, desecription, and the callable function.
+   tool_list[func.__name__] = {'description': func.__doc__, 'function': func} #stores with tool name, desecription, and the callable function.
 @tool
 def add(numbers):
    """Tool to add all # in a dict"""
@@ -32,7 +28,6 @@ def subtract(numbers):
       total -= numbers[x]
    return total
 
-
       
 #Executing Tool Calls
 #Registering tool calls
@@ -45,6 +40,13 @@ class FinalResponse(BaseModel):
     final_answer: str
 class LLMResponse(BaseModel):
     response_type: Union[ToolResponse, FinalResponse] = Field(discriminator='state')
+
+
+system_prompt = 'You are a mathematical genius. You can only respond with one of two formats: one for calling tools: {"state": "tool", "tool_name": "tool", "tool_args": {"a": x, "b": y} } or one for stating an answer: {"state": "final", "final_answer": "blah blah blah"} Respond only in JSON'
+user_prompt = 'Add 1+1 using tool'
+system_prompt = system_prompt + f'Available tools: {tool_list}'
+message_history = [{"role": "system", "content": system_prompt}, {"role": "user", "content": user_prompt}]
+
 while True:
   response = completion(
     model="mistral/mistral-tiny",
@@ -65,7 +67,16 @@ while True:
         print(result.final_answer)
         break
       if result.state =="tool":
-         
+         if result.tool_name in tool_list:
+            try:
+              tool_response = tool_list[result.tool_name]['function'](result.tool_args)
+              message_history.append({"role":"system", "content": f"Tool({result.tool_name}) Response: {tool_response}"})
+            except:
+               message_history.append({"role":"system", "content": f"Args were invalid: {result.tool_args}"})
+         else:
+            message_history.append({"role":"system", "content": f"No tool with name: {result.tool_name}"})
+        
+        
     except ValidationError as e:
       message_history.append({"role": "system", "content": e})
   except Exception as e:
--- testing.py
+++ testing.py
@@ -1,6 +1,6 @@
-tool_list = []
+tool_list = {}
 def tool(func):
-   tool_list.append({f"tool_name": func.__name__, 'description': func.__doc__, 'function': func  }) #stores with tool name, desecription, and the callable function.
+   tool_list[func.__name__] = {'description': func.__doc__, 'function': func} #stores with tool name, desecription, and the callable function.
 @tool
 def add(numbers):
    """Tool to add all # in a dict"""
