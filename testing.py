5213caf3a22d087e5aeed46593b600aa68bea958
Hunter Li <hunterisaac2009@gmail.com>
2026-05-01 19:10:38 -0700

    Working minimal rag system
    need to optimize

--- testing.py
+++ testing.py
@@ -1,14 +1,24 @@
 from langchain_text_splitters import RecursiveCharacterTextSplitter
-
-splitter = RecursiveCharacterTextSplitter(
+import os
+import chromadb 
+splitter = RecursiveCharacterTextSplitter( #taken from the docs website
     chunk_size=500,
     chunk_overlap=50,
     separators=["\n\n", "\n", ". ", " "]
 )
-import os
-arr = os.listdir('rag_docs')
+dir = 'rag_docs'
+arr = os.listdir(dir)
+client = chromadb.PersistentClient()
+collection = client.get_or_create_collection(
+    name="RAG",
+)
 for i in arr:
-    f = open(f"rag_docs/{i}")
-    chunks = splitter.split_text(f.read())
-    print(chunks)
-#chunks = splitter.split_text(document)
+    f = open(f"{dir}/{i}")
+    text = f.read()
+    chunks = splitter.split_text(text)
+    for x in range(len(chunks)):
+        collection.add(ids = f"{i}_{x}", documents=chunks[x]) #need to find way to avoid duplicates?? also weirdly overwriting what i had lol
+print(collection.query(
+    query_texts=["what is the password"], #semantic match
+    n_results=1,
+))
