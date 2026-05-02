569045cd5d1822dc9019af7f56a545b448fca240
Hunter Li <hunterisaac2009@gmail.com>
2026-05-01 19:15:55 -0700

    Optimized the rag adding embeddings
    Yep!

--- testing.py
+++ testing.py
@@ -12,12 +12,16 @@ client = chromadb.PersistentClient()
 collection = client.get_or_create_collection(
     name="RAG",
 )
+ids = []
+texts = []
 for i in arr:
     f = open(f"{dir}/{i}")
     text = f.read()
     chunks = splitter.split_text(text)
     for x in range(len(chunks)):
-        collection.add(ids = f"{i}_{x}", documents=chunks[x]) #need to find way to avoid duplicates?? also weirdly overwriting what i had lol
+        ids.append(f"{i}_{x}")
+        texts.append(chunks[x])
+collection.add(ids = ids, documents=texts) 
 print(collection.query(
     query_texts=["what is the password"], #semantic match
     n_results=1,
